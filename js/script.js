193140db2a15f8eab55511258151a2cef9ddb1c0
Islam Sayed <geosid40@outlook.com>
2023-03-11 11:30:17 +0200

    burger

--- js/script.js
+++ js/script.js
@@ -1,18 +1,28 @@
+// burger menu
+function burgerMenu() {
+    var x = document.getElementById("navLinks");
+    if (x.style.display === "flex") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "flex";
+    }
+}
+// slideshow
 let slideIndex = 0;
-        function showSlides() {
-            let i;
-            let slides = document.getElementsByClassName("mySlides");
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
 
-            for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-            }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
 
-            slideIndex++;
+    slideIndex++;
 
-            if (slideIndex > slides.length) { slideIndex = 1 }
+    if (slideIndex > slides.length) { slideIndex = 1 }
 
-            slides[slideIndex-1].style.display = "flex";
+    slides[slideIndex - 1].style.display = "flex";
 
-            setTimeout(showSlides, 7000);
-        }
-        showSlides();
+    setTimeout(showSlides, 7000);
+}
+showSlides();
